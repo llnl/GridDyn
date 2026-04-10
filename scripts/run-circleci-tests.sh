dc7b19d447c5e220e405cc499db03efb46e03491
Philip Top <phlptp@gmail.com>
2026-04-10 16:41:28 -0700

    precommit fixes

--- scripts/run-circleci-tests.sh
+++ scripts/run-circleci-tests.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 tests=(
-	/root/project/build/bin/testLibrary
-	/root/project/build/bin/testComponents
-	/root/project/build/bin/testSystem
+    /root/project/build/bin/testLibrary
+    /root/project/build/bin/testComponents
+    /root/project/build/bin/testSystem
 )
 
 SUMRESULT=0
 for test in "${tests[@]}"; do
-	echo "${test}"
-	eval "${test}"
-	RESULT=$?
-	echo "***Latest test result: "${RESULT}
-	SUMRESULT=$((SUMRESULT + RESULT))
+    echo "${test}"
+    eval "${test}"
+    RESULT=$?
+    echo "***Latest test result: "${RESULT}
+    SUMRESULT=$((SUMRESULT + RESULT))
 done
 # Return 0 or a positive integer for failure
 exit ${SUMRESULT}

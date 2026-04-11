16a7b50f47bdbac035b8b6274d78d4d2b4390ca4
Philip Top <phlptp@gmail.com>
2026-04-11 10:34:11 -0700

    update test code

--- scripts/run-circleci-tests.sh
+++ scripts/run-circleci-tests.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 REPO_ROOT=$(cd "${SCRIPT_DIR}/.." && pwd)
-tests=(
-    "${REPO_ROOT}/build/bin/testLibrary"
-    "${REPO_ROOT}/build/bin/testComponents"
-    "${REPO_ROOT}/build/bin/testSystem"
-)
 
-SUMRESULT=0
-for test in "${tests[@]}"; do
-    echo "${test}"
-    eval "${test}"
-    RESULT=$?
-    echo "***Latest test result: "${RESULT}
-    SUMRESULT=$((SUMRESULT + RESULT))
-done
-# Return 0 or a positive integer for failure
-exit ${SUMRESULT}
+ctest --test-dir "${REPO_ROOT}/build" --output-on-failure -L Continuous

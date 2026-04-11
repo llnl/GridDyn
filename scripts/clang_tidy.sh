0e61416e385d0b26c1c21dbaec4421f7b4b58395
Philip Top <phlptp@gmail.com>
2026-04-10 17:51:58 -0700

    more precommit fixes

--- scripts/clang_tidy.sh
+++ scripts/clang_tidy.sh
@@ -2,10 +2,10 @@
 # clang-tidy has segfaulted on me trying to do all the files at once; do them
 # a file at a time
 # TODO parallelize this?
-filename=../tidy/$(basename $(pwd))_$(date +%m%d%y%H%M).tidy
+filename=../tidy/$(basename "$(pwd)")_$(date +%m%d%y%H%M).tidy
 echo "Writing to $filename"
 files=$(cat ../tidy/files.txt)
 for f in $files; do
-	echo $f
-	clang-tidy $f >>$filename
+    echo "$f"
+    clang-tidy "$f" >>"$filename"
 done

bb669ad6a1a0b4e5a9965024b29061c0853fbbbd
Philip Top <phlptp@gmail.com>
2026-04-12 15:25:48 -0700

    update clang tidy

--- src/fileInput/gridDynReadDYR.cpp
+++ src/fileInput/gridDynReadDYR.cpp
@@ -8,6 +8,7 @@
 #include "core/coreObject.h"
 #include "core/objectFactory.hpp"
 #include "fileInput.h"
+#include "readerInfo.h"
 #include "gmlc/utilities/stringConversion.h"
 #include "gmlc/utilities/stringOps.h"
 #include "griddyn/Exciter.h"
@@ -17,7 +18,6 @@
 #include "griddyn/gridBus.h"
 #include <fstream>
 #include <iostream>
-#include <memory>
 #include <string>
 
 namespace griddyn {
@@ -82,140 +82,140 @@ void loadDYR(coreObject* parentObject, const std::string& fileName, const basicR
 namespace {
     void loadGENROU(coreObject* parentObject, stringVec& tokens)
     {
-        int id = std::stoi(tokens[0]);
-        gridBus* bus = static_cast<gridBus*>(parentObject->findByUserID("bus", id));
-        id = std::stoi(tokens[2]);
-        Generator* gen = bus->getGen(id - 1);
+        const int busId = std::stoi(tokens[0]);
+        const auto* bus = static_cast<gridBus*>(parentObject->findByUserID("bus", busId));
+        const int genId = std::stoi(tokens[2]);
+        auto* gen = bus->getGen(genId - 1);
 
         auto params = gmlc::utilities::str2vector(tokens, kNullVal);
 
         auto cof = coreObjectFactory::instance();
-        auto* sm = static_cast<GenModel*>(cof->createObject("genmodel", "6"));
-        sm->set("tdop", params[3]);
-        sm->set("tdopp", params[4]);
-        sm->set("tqop", params[5]);
-        sm->set("tqopp", params[6]);
-        sm->set("h", params[7]);
-        sm->set("d", params[8]);
-        sm->set("xd", params[9]);
-        sm->set("xq", params[10]);
-        sm->set("xdp", params[11]);
-        sm->set("xqp", params[12]);
-        sm->set("xdpp", params[13]);
-        sm->set("xqpp", params[13]);
-        sm->set("xl", params[14]);
-        sm->set("s1", params[15]);
-        sm->set("s12", params[16]);
+        auto* genModel = static_cast<GenModel*>(cof->createObject("genmodel", "6"));
+        genModel->set("tdop", params[3]);
+        genModel->set("tdopp", params[4]);
+        genModel->set("tqop", params[5]);
+        genModel->set("tqopp", params[6]);
+        genModel->set("h", params[7]);
+        genModel->set("d", params[8]);
+        genModel->set("xd", params[9]);
+        genModel->set("xq", params[10]);
+        genModel->set("xdp", params[11]);
+        genModel->set("xqp", params[12]);
+        genModel->set("xdpp", params[13]);
+        genModel->set("xqpp", params[13]);
+        genModel->set("xl", params[14]);
+        genModel->set("s1", params[15]);
+        genModel->set("s12", params[16]);
 
-        gen->add(sm);
+        gen->add(genModel);
     }
 
     void loadESDC1A(coreObject* parentObject, stringVec& tokens)
     {
-        int id = std::stoi(tokens[0]);
-        gridBus* bus = static_cast<gridBus*>(parentObject->findByUserID("bus", id));
-        id = std::stoi(tokens[2]);
-        Generator* gen = bus->getGen(id - 1);
+        const int busId = std::stoi(tokens[0]);
+        const auto* bus = static_cast<gridBus*>(parentObject->findByUserID("bus", busId));
+        const int genId = std::stoi(tokens[2]);
+        auto* gen = bus->getGen(genId - 1);
 
         auto params = gmlc::utilities::str2vector(tokens, kNullVal);
-        Exciter* sm;
+        Exciter* exciterModel;
         auto cof = coreObjectFactory::instance();
         if (params[6] > 0.0)  // dc1a model must have tb>0 otherwise revert to type1
         {
-            sm = static_cast<Exciter*>(cof->createObject("exciter", "dc1a"));
+            exciterModel = static_cast<Exciter*>(cof->createObject("exciter", "dc1a"));
         } else {
-            sm = static_cast<Exciter*>(cof->createObject("exciter", "type1"));
+            exciterModel = static_cast<Exciter*>(cof->createObject("exciter", "type1"));
         }
         // TODO(phlpt): TR not implemented yet; no voltage compensation implemented.
-        // sm->set("tr", params[3]);
-        sm->set("ka", params[4]);
-        sm->set("ta", params[5]);
+        // exciterModel->set("tr", params[3]);
+        exciterModel->set("ka", params[4]);
+        exciterModel->set("ta", params[5]);
         if (params[6] > 0) {
-            sm->set("tb", params[6]);
-            sm->set("tc", params[7]);
+            exciterModel->set("tb", params[6]);
+            exciterModel->set("tc", params[7]);
         }
-        sm->set("vrmax", params[8]);
-        sm->set("vrmin", params[9]);
-        sm->set("ke", params[10]);
-        sm->set("te", params[11]);
-        sm->set("kf", params[12]);
-        sm->set("tf", params[13]);
+        exciterModel->set("vrmax", params[8]);
+        exciterModel->set("vrmin", params[9]);
+        exciterModel->set("ke", params[10]);
+        exciterModel->set("te", params[11]);
+        exciterModel->set("kf", params[12]);
+        exciterModel->set("tf", params[13]);
         // TODO(phlpt): Compute the saturation coefficients to translate appropriately.
 
-        gen->add(sm);
+        gen->add(exciterModel);
     }
 
     void loadEXDC2(coreObject* parentObject, stringVec& tokens)
     {
-        int id = std::stoi(tokens[0]);
-        gridBus* bus = static_cast<gridBus*>(parentObject->findByUserID("bus", id));
-        id = std::stoi(tokens[2]);
-        Generator* gen = bus->getGen(id - 1);
+        const int busId = std::stoi(tokens[0]);
+        const auto* bus = static_cast<gridBus*>(parentObject->findByUserID("bus", busId));
+        const int genId = std::stoi(tokens[2]);
+        auto* gen = bus->getGen(genId - 1);
 
         auto params = gmlc::utilities::str2vector(tokens, kNullVal);
 
         auto cof = coreObjectFactory::instance();
-        auto* sm = static_cast<Exciter*>(cof->createObject("exciter", "dc2a"));
+        auto* exciterModel = static_cast<Exciter*>(cof->createObject("exciter", "dc2a"));
         // TODO(phlpt): TR not implemented yet; no voltage compensation implemented.
-        // sm->set("tr", params[3]);
-        sm->set("ka", params[4]);
-        sm->set("ta", params[5]);
-        sm->set("tb", params[6]);
-        sm->set("tc", params[7]);
-        sm->set("vrmax", params[8]);
-        sm->set("vrmin", params[9]);
-        sm->set("ke", params[10]);
-        sm->set("te", params[11]);
-        sm->set("kf", params[12]);
-        sm->set("tf", params[13]);
+        // exciterModel->set("tr", params[3]);
+        exciterModel->set("ka", params[4]);
+        exciterModel->set("ta", params[5]);
+        exciterModel->set("tb", params[6]);
+        exciterModel->set("tc", params[7]);
+        exciterModel->set("vrmax", params[8]);
+        exciterModel->set("vrmin", params[9]);
+        exciterModel->set("ke", params[10]);
+        exciterModel->set("te", params[11]);
+        exciterModel->set("kf", params[12]);
+        exciterModel->set("tf", params[13]);
         // TODO(phlpt): Compute the saturation coefficients to translate appropriately.
 
-        gen->add(sm);
+        gen->add(exciterModel);
     }
 
     void loadSEXS(coreObject* parentObject, stringVec& tokens)
     {
-        int id = std::stoi(tokens[0]);
-        gridBus* bus = static_cast<gridBus*>(parentObject->findByUserID("bus", id));
-        id = std::stoi(tokens[2]);
-        Generator* gen = bus->getGen(id - 1);
+        const int busId = std::stoi(tokens[0]);
+        const auto* bus = static_cast<gridBus*>(parentObject->findByUserID("bus", busId));
+        const int genId = std::stoi(tokens[2]);
+        auto* gen = bus->getGen(genId - 1);
 
         auto params = gmlc::utilities::str2vector(tokens, kNullVal);
         auto cof = coreObjectFactory::instance();
-        auto* sm = static_cast<Exciter*>(cof->createObject("exciter", "sexs"));
+        auto* exciterModel = static_cast<Exciter*>(cof->createObject("exciter", "sexs"));
 
-        // sm->set("tr", params[3]);
-        sm->set("ka", params[5]);
-        sm->set("tb", params[4]);
-        sm->set("ta", params[3] * params[4]);
-        sm->set("te", params[6]);
-        sm->set("vrmax", params[8]);
-        sm->set("vrmin", params[7]);
+        // exciterModel->set("tr", params[3]);
+        exciterModel->set("ka", params[5]);
+        exciterModel->set("tb", params[4]);
+        exciterModel->set("ta", params[3] * params[4]);
+        exciterModel->set("te", params[6]);
+        exciterModel->set("vrmax", params[8]);
+        exciterModel->set("vrmin", params[7]);
 
-        gen->add(sm);
+        gen->add(exciterModel);
     }
     void loadTGOV1(coreObject* parentObject, stringVec& tokens)
     {
-        int id = std::stoi(tokens[0]);
-        gridBus* bus = static_cast<gridBus*>(parentObject->findByUserID("bus", id));
-        id = std::stoi(tokens[2]);
-        Generator* gen = bus->getGen(id - 1);
+        const int busId = std::stoi(tokens[0]);
+        const auto* bus = static_cast<gridBus*>(parentObject->findByUserID("bus", busId));
+        const int genId = std::stoi(tokens[2]);
+        auto* gen = bus->getGen(genId - 1);
 
         auto params = gmlc::utilities::str2vector(tokens, kNullVal);
 
         auto cof = coreObjectFactory::instance();
-        auto* sm = static_cast<Governor*>(cof->createObject("governor", "tgov1"));
+        auto* governorModel = static_cast<Governor*>(cof->createObject("governor", "tgov1"));
         // TODO(phlpt): TR not implemented yet; no voltage compensation implemented.
-        // sm->set("tr", params[3]);
-        sm->set("r", params[3]);
-        sm->set("t1", params[4]);
-        sm->set("pmax", params[5]);
-        sm->set("pmin", params[6]);
-        sm->set("t2", params[6]);
-        sm->set("t3", params[7]);
-        sm->set("dt", params[8]);
+        // governorModel->set("tr", params[3]);
+        governorModel->set("r", params[3]);
+        governorModel->set("t1", params[4]);
+        governorModel->set("pmax", params[5]);
+        governorModel->set("pmin", params[6]);
+        governorModel->set("t2", params[6]);
+        governorModel->set("t3", params[7]);
+        governorModel->set("dt", params[8]);
 
-        gen->add(sm);
+        gen->add(governorModel);
     }
 }  // namespace
 

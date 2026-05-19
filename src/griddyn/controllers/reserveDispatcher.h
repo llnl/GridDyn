13e89aa2609bf0573578879265fab287b131cfc6
Philip Top <phlptp@gmail.com>
2026-05-19 05:36:28 -0700

    clang-tidy

--- src/griddyn/controllers/reserveDispatcher.h
+++ src/griddyn/controllers/reserveDispatcher.h
@@ -39,7 +39,7 @@ class reserveDispatcher: public CoreObject {
 
     virtual double dynInitializeA(coreTime time0, double dispatchSet);
 
-    void moveSchedulers(reserveDispatcher* rD);
+    void moveSchedulers(reserveDispatcher* dispatcherToMove);
 
     virtual double updateP(coreTime time, double pShort);
     virtual double testP(coreTime time, double pShort);
